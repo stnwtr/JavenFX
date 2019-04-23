5f00efa05b0334b5bc429a8a52825848315ec804
stnwtr <simon.steinkellner.ss@gmail.com>
2019-04-23 09:48:38 +0200

    Added wildcard to generic event type

--- src/main/java/net/felium/javenfx/FXEventType.java
+++ src/main/java/net/felium/javenfx/FXEventType.java
@@ -4,19 +4,53 @@ import javafx.event.EventType;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 
+/**
+ * https://github.com/stnwtr/
+ *
+ * @author stnwtr
+ * @since 22.04.2019
+ */
 public enum FXEventType {
+    /**
+     * Fired on any mouse event.
+     */
     MOUSE_EVENT_ANY(MouseEvent.ANY),
+
+    /**
+     * Fired on when mouse is clicked.
+     */
     MOUSE_EVENT_MOUSE_CLICKED(MouseEvent.MOUSE_CLICKED),
+
+    /**
+     * Fired when mouse is moved.
+     */
     MOUSE_EVENT_MOUSE_MOVED(MouseEvent.MOUSE_MOVED),
+
+    /**
+     * Fired when a key is pressed.
+     */
     KEY_EVENT_KEY_PRESSED(KeyEvent.KEY_PRESSED);
 
-    private EventType type;
+    /**
+     * The {@link EventType} of the event.
+     */
+    private EventType<?> type;
 
-    FXEventType(EventType type) {
+    /**
+     * Simple constructor for event type.
+     *
+     * @param type The raw event type.
+     */
+    FXEventType(EventType<?> type) {
         this.type = type;
     }
 
-    public EventType getType() {
+    /**
+     * Get the event type out of the enum.
+     *
+     * @return The raw event type.
+     */
+    public EventType<?> getType() {
         return type;
     }
 }
